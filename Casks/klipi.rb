54a76cd990c8f842b2c8fef5cd15f673e60e1322
prolifel <clement.085w@gmail.com>
2026-04-04 11:01:22 +0700

    klipi: fix error order

--- Casks/klipi.rb
+++ Casks/klipi.rb
@@ -2,7 +2,7 @@ cask "klipi" do
   version "1.0.16"
   sha256 "e4746ab45ea29db24fb4ea5e42d26df3d5e97b341cbd84a75cd3486c586fc29b"
 
-  url "https://github.com/prolifel/klipi/releases/download/v\#{version}/Klipi-\#{version}.dmg"
+  url "https://github.com/prolifel/klipi/releases/download/v#{version}/Klipi-#{version}.dmg"
   name "Klipi"
   desc "Lightweight clipboard manager for menu bar"
   homepage "https://github.com/prolifel/klipi"
@@ -14,15 +14,28 @@ cask "klipi" do
 
   app "Klipi.app"
 
+  postflight do
+    app_link = "/Applications/Klipi.app"
+    File.unlink(app_link) if File.symlink?(app_link)
+    File.symlink(appdir/"Klipi.app", app_link)
+  end
+
+  uninstall quit: "com.klipi.app"
+
+  zap trash: [
+    "~/Library/Application Support/Klipi",
+    "~/Library/Preferences/com.klipi.app.plist",
+  ]
+
   caveats do
-    s = "Klipi has been installed to \#{appdir}/Klipi.app\n\n"
+    s = "Klipi has been installed to #{appdir}/Klipi.app\n\n"
 
     if File.symlink?("/Applications/Klipi.app")
       s += "A symlink has been created in /Applications.\n\n"
     else
       s += "\e[33mCould not create symlink in /Applications.\e[0m\n"
       s += "To add to Applications manually, run:\n"
-      s += "  ln -sf \#{appdir}/Klipi.app /Applications/Klipi.app\n\n"
+      s += "  ln -sf #{appdir}/Klipi.app /Applications/Klipi.app\n\n"
     end
 
     s += "\e[32m==> IMPORTANT: Grant Input Monitoring permission!\e[0m\n"
@@ -35,17 +48,4 @@ cask "klipi" do
     s += "To uninstall: run \e[34mklipi-uninstall\e[0m\n"
     s
   end
-
-  postflight do
-    app_link = "/Applications/Klipi.app"
-    FileUtils.rm_f(app_link)
-    FileUtils.ln_sf(appdir/"Klipi.app", app_link)
-  end
-
-  uninstall quit: "com.klipi.app"
-
-  zap trash: [
-    "~/Library/Application Support/Klipi",
-    "~/Library/Preferences/com.klipi.app.plist",
-  ]
 end
